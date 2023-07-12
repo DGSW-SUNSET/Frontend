241a2a93a8a375b98c924c3b2a846c765c0495ea
김수인 <kim061222@gmail.com>
2023-07-12 16:38:22 +0900

    :white_check_mark:
    maybe, I finish Main....

--- dive-sunset/src/components/Main/main.jsx
+++ dive-sunset/src/components/Main/main.jsx
@@ -28,10 +28,6 @@ function Main() {
                         </div>
                         <div className="info">
                             <img src = {complete}></img>
-                            <div className="heart">
-                                <img src = {heart}></img>
-                                0
-                            </div>
                             <div className="comment">
                                 <img src = {comment}></img>
                                 0
@@ -41,13 +37,43 @@ function Main() {
                 </div>
                 <div className="looking">
                     찾는 중인 교환글
+                    <div className = "writebox">
+                        <div className="textbox">
+                            <div className="title">인형 교환하실 분!</div>
+                            <img src = {dummyimg} className = "writeboxImg"></img>
+                            저 : 사진 속 강아지 인형 <br></br>
+                            님 : 고양이 인형이면 OK
+                        </div>
+                        <div className="info">
+                            <img src = {complete}></img>
+                            <div className="comment">
+                                <img src = {comment}></img>
+                                0
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="complete">
                     완료된 교환글
+                    <div className = "writebox">
+                        <div className="textbox">
+                            <div className="title">인형 교환하실 분!</div>
+                            <img src = {dummyimg} className = "writeboxImg"></img>
+                            저 : 사진 속 강아지 인형 <br></br>
+                            님 : 고양이 인형이면 OK
+                        </div>
+                        <div className="info">
+                            <img src = {complete}></img>
+                            <div className="comment">
+                                <img src = {comment}></img>
+                                0
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <Link to = "/ShowAll">
-                <img src = {showAllButton}></img>
+                <img src = {showAllButton} className="showAllButton"></img>
             </Link>
             
         </>
