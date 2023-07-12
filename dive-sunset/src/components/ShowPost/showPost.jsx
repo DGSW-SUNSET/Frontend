091aaa503e6037b0d0fee8689299f98d36a7818d
김수인 <kim061222@gmail.com>
2023-07-12 21:57:09 +0900

    :fire:
    save

---
+++ dive-sunset/src/components/ShowPost/showPost.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "../Header/header";
+
+function ShowPost() {
+    return(
+        <>
+            <Header></Header>
+        </>
+    )
+}
+export default ShowPost;
